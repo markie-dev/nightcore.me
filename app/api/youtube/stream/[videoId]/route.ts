3846826c6489ec3df95c7265567c7a4afa6fe89b
marcus <markie@markie.dev>
2025-01-27 00:58:11 -0600

    trying no cookies

--- app/api/youtube/stream/[videoId]/route.ts
+++ app/api/youtube/stream/[videoId]/route.ts
@@ -2,26 +2,21 @@
 import { NextResponse } from 'next/server';
 import ytdl from '@distube/ytdl-core';
 
-const cookies = process.env.YOUTUBE_COOKIES 
-  ? JSON.parse(process.env.YOUTUBE_COOKIES)
-  : [];
+const cookies = [];
 
 const requestOptions = {
-  agent: ytdl.createAgent(cookies),
+  agent: ytdl.createAgent(),
   headers: {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.9',
     'Accept-Encoding': 'gzip, deflate, br',
     'Connection': 'keep-alive',
-    'Cookie': cookies.map((cookie: any) => `${cookie.name}=${cookie.value}`).join('; '),
     'Referer': 'https://www.youtube.com/',
     'Origin': 'https://www.youtube.com'
   }
 };
 
-console.log('Number of cookies loaded:', cookies.length);
-console.log('Cookie names loaded:', cookies.map((c: { name: string }) => c.name));
 
 export async function GET(req: Request, context: any) {
   const { videoId } = await context.params;
